537772ae84d6f9e3b1f52369fddfca4d107170b4
seoyoon04 <sykim8483@naver.com>
2022-06-23 23:55:06 +0900

    first commit

---
+++ app/src/main/java/com/example/foodapp/Activity/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.foodapp.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.example.foodapp.Adaptor.CategoryAdaptor;
+import com.example.foodapp.Domain.CategoryDomain;
+import com.example.foodapp.R;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView.Adapter adapter;
+    private RecyclerView recyclerViewCategoryList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerViewCategory();
+    }
+
+    private void recyclerViewCategory() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerViewCategoryList = findViewById(R.id.recyclerView);
+        recyclerViewCategoryList.setLayoutManager(linearLayoutManager);
+
+        ArrayList<CategoryDomain> category = new ArrayList<>();
+        category.add(new CategoryDomain("Pizza", "cat_1"));
+        category.add(new CategoryDomain("Burger", "cat_2"));
+        category.add(new CategoryDomain("Hotdog", "cat_3"));
+        category.add(new CategoryDomain("Drink", "cat_4"));
+        category.add(new CategoryDomain("Donut", "cat_5"));
+
+        adapter = new CategoryAdaptor(category);
+        recyclerViewCategoryList.setAdapter(adapter);
+
+
+    }
+}
